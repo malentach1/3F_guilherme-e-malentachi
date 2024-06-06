051664407bc1a2d7fc5e36510a9f30050029010f
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-06-06 09:48:30 -0300

    06.06

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
+const abaconteudo = document.querySelectorAll(".abaconteudo");
 
-for (let i = 0; i < botoes.length ; i++){
+
+for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
-        for (let j = 0 ; j < botoes.length ; j++){
+        for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo")
+            abaconteudo[j].classList.remove("ativo")
+            
         }
         botoes[i].classList.add("ativo")
+        abaconteudo[i].classList.add("ativo")
     }
 }
