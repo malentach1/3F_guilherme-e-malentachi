dafce285c43d5da84e6967615a038bb36eeded10
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-06-14 08:53:33 -0300

    Add files via upload
    14.06

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
-const abaconteudo = document.querySelectorAll(".abaconteudo");
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date(2024,7)
+const tempoObjetivo2 = new Date(2024,8)
+const tempoObjetivo3 = new Date(2024,9)
+const tempoObjetivo4 = new Date(2024,10)
 
-
-for (let i = 0; i < botoes.length; i++){
-    botoes[i].onclick = function(){
-        for (let j = 0; j < botoes.length; j++){
-            botoes[j].classList.remove("ativo")
-            abaconteudo[j].classList.remove("ativo")
-            
+for (let i = 0 ; i < botoes.length ; i++){
+    botoes[i].onclick = function (){
+        for (let j = 0; j < botoes.length ; j++){
+            botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo")
-        abaconteudo[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
+        conteudo[i].classList.add("ativo");
     }
 }
