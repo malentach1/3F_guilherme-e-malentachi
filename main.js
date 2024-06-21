c9d7f654d009a9e414d1349ad00fd236ec9c5fc9
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-06-21 08:57:47 -0300

    Add files via upload
    21.06

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
+const conteudo = document.querySelectorAll(".abaconteudo");
 const contador = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(2024,7)
-const tempoObjetivo2 = new Date(2024,8)
-const tempoObjetivo3 = new Date(2024,9)
-const tempoObjetivo4 = new Date(2024,10)
+const tempoObjetivo1 = new Date (2025,02,25);
+const tempoObjetivo2 = new Date (2025,05,25);
+const tempoObjetivo3 = new Date (2025,08,25);
+const tempoObjetivo4 = new Date (2025,10,25);
+let agora = new Date();
+contador[0].textContent = tempoObjetivo1 - agora
+contador[1].textContent = tempoObjetivo2 - agora
+contador[2].textContent = tempoObjetivo3 - agora
+contador[3].textContent = tempoObjetivo4 - agora
 
-for (let i = 0 ; i < botoes.length ; i++){
-    botoes[i].onclick = function (){
-        for (let j = 0; j < botoes.length ; j++){
-            botoes[j].classList.remove("ativo");
-            conteudo[j].classList.remove("ativo");
+let segundos
+let minutos 
+let horas 
+let dias 
+
+segundos = (tempoObjetivo1-agora)/1000;
+minutos = segundos/60
+horas= minutos/60
+dias= horas/24
+
+contador[0].textContent = `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos}segundos`
+
+
+for (let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+        for (let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo")
+            conteudo[j].classList.remove("ativo")
+            
         }
-        botoes[i].classList.add("ativo");
-        conteudo[i].classList.add("ativo");
+        botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
+
+
     }
 }
