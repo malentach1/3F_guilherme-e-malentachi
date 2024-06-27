9a940db9ab32bba02f52eedc51e4588288e2939a
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-06-27 09:46:34 -0300

    Add files via upload
    27.06

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abaconteudo");
 const contador = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date (2025,02,25);
-const tempoObjetivo2 = new Date (2025,05,25);
-const tempoObjetivo3 = new Date (2025,08,25);
-const tempoObjetivo4 = new Date (2025,10,25);
-let agora = new Date();
-contador[0].textContent = tempoObjetivo1 - agora
-contador[1].textContent = tempoObjetivo2 - agora
-contador[2].textContent = tempoObjetivo3 - agora
-contador[3].textContent = tempoObjetivo4 - agora
 
-let segundos
-let minutos 
-let horas 
-let dias 
+const tempoObjetivo1 = new Date (2025,6,16);
+const tempoObjetivo2 = new Date (2025,7,16);
+const tempoObjetivo3 = new Date (2025,8,16);
+const tempoObjetivo4 = new Date (2025,10,16);
 
-segundos = (tempoObjetivo1-agora)/1000;
-minutos = segundos/60
-horas= minutos/60
-dias= horas/24
-
-contador[0].textContent = `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos}segundos`
-
+function calculaTempo(tempoObjetivo){
+    const agora = new Date()
+    let segundos
+    let minutos 
+    let horas
+    let dias 
+    segundos = (tempoObjetivo-agora)/1000
+    minutos = segundos/60
+    horas = minutos/60
+    dias = horas/24
+    segundos = Math.floor(segundos);
+    minutos = Math.floor(minutos);
+    horas = Math.floor(horas);
+    dias = Math.floor(dias);
+}
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
